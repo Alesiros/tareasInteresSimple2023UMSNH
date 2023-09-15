e39202916d56a4d2dd76f1fbfd6392380bef9178
Alesiros <144080328+Alesiros@users.noreply.github.com>
2023-09-14 19:07:47 -0600

    Add files via upload

---
+++ sumaTerminoNSucAritmetica.R
@@ -0,0 +1,6 @@
+sumaTerminoNSucAritmetica=function(x1, xn, n){
+  sn=((n/2)*(x1+xn))
+  print(
+    paste0("Suma hasta el termino " ,n, " de la sucesión es: " ,sn, " , con un termino inicial de " ,x1, " y un termino final de " ,xn)
+  )
+}
